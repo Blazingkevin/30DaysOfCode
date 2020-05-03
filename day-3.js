da0ea2714b318faeefe56c49f99cd48bba3a90a0
Kevin Uti <kvuti10@gmail.com>
2020-05-03 22:10:17 +0100

    updated for day-3

--- day-3.js
+++ day-3.js
@@ -4,14 +4,13 @@
  * @param {Number} num number that will be formatted by adding leading zeros
  */
 const myPackaging = (size, num)=> {
-    //case where num has leadind zeros e.g 023, remove the zeros
     if(String(num).length >= size) return String(num);
     /** 
-    create an empty array of the same size as the expeted final length of the resulting string
+    create an empty array of the same size as the expected final length of the resulting string,
     fill the array with dummy value, "0" ..create a new array by mapping through the zero-filled array and
     return zero by default during the mapping until you get to a breaking_index, which is the index at which
     we have gotten the number of zeros we need. Any iteration after this breaking_index during the mapping, should
-    return charaters from the "num" arguement starting from the first character.
+    return characters from the "num" arguement starting from the first character.
     At the end of it all we have array of characters in the required order. A join method does the final trick.
     NB: breaking_index is saved in the empty object(second arguement of the map function) 
     */
